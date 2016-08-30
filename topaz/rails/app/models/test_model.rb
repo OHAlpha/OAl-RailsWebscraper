8865d9c1bbe482262c027f87afaca8b6fb18331d
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-08-30 21:34:41 +0000

    created model TestModel.

---
+++ topaz/rails/app/models/test_model.rb
@@ -0,0 +1,12 @@
+class TestModel < ActiveRecord::Base
+    
+    #attr_accessor :attr1
+    attr_accessible :attr1
+    #attr_accessor :attr2
+    attr_accessible :attr2
+    
+    private
+        
+        
+        
+end
