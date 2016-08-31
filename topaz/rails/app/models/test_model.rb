bc4af38b3ff589d85aa76546009b7b617ae87d2c
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-08-31 14:26:41 +0000

    working on a model/concerns module

--- topaz/rails/app/models/test_model.rb
+++ topaz/rails/app/models/test_model.rb
@@ -1,12 +1,5 @@
 class TestModel < ActiveRecord::Base
     
-    #attr_accessor :attr1
-    attr_accessible :attr1
-    #attr_accessor :attr2
-    attr_accessible :attr2
+    include PseudoAttributes
     
-    private
-        
-        
-        
 end
