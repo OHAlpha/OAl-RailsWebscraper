9a3d5f80c1faff16105d367a8379fdf692a8d41f
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-09-13 19:23:52 +0000

    modified document and content to include is_standalone and usage_type respectively

--- topaz/rails-5.0/db/migrate/20160913190056_create_content_documents.rb
+++ topaz/rails-5.0/db/migrate/20160913190056_create_content_documents.rb
@@ -1,6 +1,7 @@
 class CreateContentDocuments < ActiveRecord::Migration[5.0]
   def change
     create_table :content_documents do |t|
+      t.integer :is_standalone
       t.integer :avenue_id
       t.integer :source_id
       t.string :source_type
--- topaz/rails-5.0/db/migrate/20160913190239_create_content_contents.rb
+++ topaz/rails-5.0/db/migrate/20160913190239_create_content_contents.rb
@@ -1,6 +1,7 @@
 class CreateContentContents < ActiveRecord::Migration[5.0]
   def change
     create_table :content_contents do |t|
+      t.integer :usage_type
       t.integer :avenue_id
       t.integer :source_id
       t.string :source_type
