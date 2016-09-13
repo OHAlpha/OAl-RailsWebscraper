2e1af097de8393a62bbf9580d307df54ed382961
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-09-13 19:55:06 +0000

    added Network::Content::ContentUse model

--- topaz/rails-5.0/db/migrate/20160913194927_create_content_content_uses.rb
+++ topaz/rails-5.0/db/migrate/20160913194927_create_content_content_uses.rb
@@ -2,7 +2,7 @@ class CreateContentContentUses < ActiveRecord::Migration[5.0]
   def change
     create_table :content_content_uses do |t|
       t.integer :document_id
-      t.string :content_id
+      t.integer :content_id
 
       t.timestamps
     end
