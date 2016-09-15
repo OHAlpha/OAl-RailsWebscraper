b1e2f888a4aa4aa22184da7afbc1dd959117cf3d
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-09-15 20:48:32 +0000

    add initial queries to each scraper route corresponding to single instances

--- topaz/rails-5.0/app/controllers/scraper_controller.rb
+++ topaz/rails-5.0/app/controllers/scraper_controller.rb
@@ -28,62 +28,81 @@ class ScraperController < ApplicationController
         end
       end
     end
+=begin
     avenue.transaction do
       if avenue.
     end
+=end
   end
 
   def avenue
+    @avenue = Network::Avenue.find params[:avenue_id]
   end
 
   def access
+    @access = Network::Access.find params[:access_id]
   end
 
   def access_response
+    @access = Network::Access.find params[:access_id]
   end
 
   def helper_sets
+    @helper_sets = Network::HeaderSet.all
   end
 
   def helper_set
+    @helper_set = Network::HeaderSet.find params[:helper_set_id]
   end
 
   def documents
+    @documents = Content::Document.all
   end
 
   def document
+    @document = Content::Document.find params[:document_id]
   end
 
   def document_data
+    @document = Content::Document.find params[:document_id]
   end
 
   def contents
+    @contents = Content::Content.all
   end
 
   def content
+    @content = Content::Content.find params[content_id]
   end
 
   def content_data
+    @content = Content::Content.find params[content_id]
   end
 
   def pages
+    @pages = Content::HtmlDocument.all
   end
 
   def page_map
   end
 
   def page
+    @page = Content::HtmlDocument.find params[page_id]
   end
 
   def page_data
+    @page = Content::HtmlDocument.find params[page_id]
   end
 
   def images
+    @image = Content::Image.all
   end
 
   def image
+    @image = Content::Image.find params[image_id]
   end
 
   def image_data
+    @image = Content::Image.find params[image_id]
   end
 end
